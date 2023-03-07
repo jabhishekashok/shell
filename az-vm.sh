ee079d162cf28bfc98abd8ba147f2d12c466b677
CAESAR269 <102550325+CAESAR269@users.noreply.github.com>
2023-03-07 19:26:46 +0530

    Create az-vm.sh

---
+++ az-vm.sh
@@ -0,0 +1,44 @@
+az group create \
+--name AZ \
+--location eastus
+
+az network nsg create \
+--resource-group AZ \
+--name AZ-NSG
+
+az network nsg rule create \
+--resource-group AZ \
+--nsg-name AZ-NSG --name AZ_Nsg_Rule --priority 100 \
+--source-address-prefixes '*' --source-port-ranges 22 \
+--destination-address-prefixes '*' --destination-port-ranges '*' \
+--access Allow --protocol Tcp --description "Allowing SSH"
+
+for i in `seq 1  `; do
+   az vm create \
+     --resource-group AZ \
+     --name Master$i \
+     --size Standard_B1s  \
+     --storage-sku StandardSSD_LRS \
+     --os-disk-size-gb 32 \
+     --vnet-name Vnet \
+     --subnet Subnet \
+     --nsg AZ-Nsg \
+     --image UbuntuLTS \
+     --admin-username master \
+     --admin-password "india@123456"
+done
+
+for i in `seq 2  `; do
+   az vm create \
+     --resource-group AZ \
+     --name Node$i \
+     --size Standard_B1s  \
+     --storage-sku StandardSSD_LRS \
+     --os-disk-size-gb 32 \
+     --vnet-name Vnet \
+     --subnet Subnet \
+     --nsg AZ-Nsg \
+     --image UbuntuLTS \
+     --admin-username node \
+     --admin-password "india@123456"
+done
