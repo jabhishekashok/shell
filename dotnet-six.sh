9f4c01dd216d1f084328afb8343afb1f292be1b9
CAESAR269 <102550325+CAESAR269@users.noreply.github.com>
2023-03-08 12:11:53 +0530

    Update dotnet-six.sh

--- dotnet-six.sh
+++ dotnet-six.sh
@@ -1,4 +1,4 @@
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install openjdk-17-jdk -y
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
