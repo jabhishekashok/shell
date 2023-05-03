10d759e9805d24afc4c34a7a69bd907348c660a4
CAESAR269 <102550325+CAESAR269@users.noreply.github.com>
2023-05-03 12:27:21 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,2 +1,6 @@
+sudo apt-get update -y
 curl -sSL https://get.docker.com/ | sh
-sudo chmod 666 /var/run/docker.sock
+sudo chmod 777 /var/run/docker.sock
+sudo apt-get install openjdk-17-jdk -y
+sudo apt-get install openjdk-11-jdk -y
+sudo apt-get install openjdk-8-jdk -y
