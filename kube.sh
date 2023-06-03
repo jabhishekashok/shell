7cb5b70aff0e95a62a39b9976f52adf86b0267d0
CAESAR269 <102550325+CAESAR269@users.noreply.github.com>
2023-06-03 13:23:34 +0530

    Update kube.sh

--- kube.sh
+++ kube.sh
@@ -1,5 +1,5 @@
 az group create --name myResourceGroup --location eastus
 
-az aks create -g myResourceGroup -n myAKSCluster --enable-managed-identity --node-count 2 --node-vm-size Standard_B2S --enable-addons monitoring --enable-msi-auth-for-monitoring  --generate-ssh-keys
+az aks create -g myResourceGroup -n myAKSCluster --enable-managed-identity --node-count 1 --node-vm-size Standard_B2S --enable-addons monitoring --enable-msi-auth-for-monitoring  --generate-ssh-keys
 
 az aks get-credentials --resource-group myResourceGroup --name myAKSCluster
