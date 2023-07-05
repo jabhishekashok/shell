edd924f65405333a521b160bef024f27ea225693
jabhishekashok <46386368+jabhishekashok@users.noreply.github.com>
2023-07-05 16:15:52 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -2,10 +2,10 @@ sudo apt-get update
 sudo apt-get install openjdk-17-jdk -y
 sudo apt-get install openjdk-8-jdk -y
 sudo apt-get install openjdk-11-jdk -y
-sudo wget https://dlcdn.apache.org/maven/maven-3/3.9.0/binaries/apache-maven-3.9.0-bin.tar.gz
-sudo tar xzvf apache-maven-3.9.0-bin.tar.gz
+sudo wget https://dlcdn.apache.org/maven/maven-3/3.9.3/binaries/apache-maven-3.9.3-bin.tar.gz
+sudo tar xzvf apache-maven-3.9.3-bin.tar.gz
 
-sudo mv apache-maven-3.9.0  /opt/maven/
+sudo mv apache-maven-3.9.3  /opt/maven/
 
-export PATH="/opt/maven/apache-maven-3.9.0/bin:$PATH"
+export PATH="/opt/maven/apache-maven-3.9.3/bin:$PATH"
 
